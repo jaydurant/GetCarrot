888ce3d5668a194f3e5239202bfb6c02f9d17f0f
durantjay <durantjay1@gmail.com>
2016-03-16 18:31:38 -0700

    Add jest/react testing

---
+++ app/js/__tests__/user-button.js
@@ -0,0 +1,32 @@
+jest.unmock('../components/user-button');
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import TestUtils  from 'react-addons-test-utils';
+import UserButton from '../components/user-button';
+
+const fields = {
+	next: '3',
+	onchoice: () => {},
+	datakey: '1',
+	text: 'hey'
+};
+
+describe('UserButton', () => {
+	it('displays user button content with next parameters', () => {
+		const renderer = TestUtils.createRenderer();
+		renderer.render(<UserButton {...fields} />);
+		const renderedTree = renderer.getRenderOutput();
+
+		console.log(renderedTree.props.type);
+
+		expect(renderedTree.type).toBe('button');
+
+		expect(renderedTree.props.children).toBe(fields.text);
+
+		expect(typeof renderedTree.props.onClick).toBe('function');
+
+		expect(renderedTree.props['data-next']).toBe(fields.next);
+
+	});
+});
